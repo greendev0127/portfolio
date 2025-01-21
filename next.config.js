32d1cef3ef6bb8503f6a245b14299da338056dce
greendev0127 <daniellee920127@gmail.com>
2025-01-21 13:31:39 -0500

    disable eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   env: {
     EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
     EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID,
