87abcf7928b1aa48fb8c9ee1a87e04e8b8a46e27
greendev0127 <daniellee920127@gmail.com>
2025-01-21 13:20:41 -0500

    first commit

---
+++ src/mock/profile.js
@@ -0,0 +1,27 @@
+export const SOCIAL_LINKS = [
+  {
+    link: 'https://twitter.com/dhavalCode',       
+    icon: 'akar-icons:twitter-fill',        
+  },
+  {
+    link: 'https://linkedin.com/in/dhavalcode',
+    icon: 'akar-icons:facebook-fill',
+  },
+  {
+    link: 'https://github.com/dhavalcode',
+    icon: 'akar-icons:github-fill',
+  },
+  {
+    link: 'https://linkedin.com/in/dhavalcode',
+    icon: 'akar-icons:linkedin-fill',
+  },
+];
+
+export const HERO_TITLES = ['Benjamin Emerson', 'Full-stack Engineer', 'SaaS Expert'];
+
+export const aboutParagraph =
+  'A Senior full stack engineer with 11+ years of experience in software development, Work experience in 4 large companies, Developed and Launched 30+ web and mobile applications';
+
+export const contactEmail = 'works.dhaval@gmail.com';
+
+export const documentTitle = 'Hello I am Dhaval Patel | Full Stack Engineer | AI Enthusiast'
